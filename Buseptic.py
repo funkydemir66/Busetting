e2f557a214f163ac18b5a86d662cf0e8c8680ced
Demir <95206179+funkydemir66@users.noreply.github.com>
2022-08-12 21:40:20 +0300

    vaay
    hahaha

---
+++ Buseptic.py
@@ -0,0 +1,25 @@
+import sys
+from g_python.gextension import Extension
+from g_python.hmessage import Direction
+
+
+extension_info = {
+    "title": "Buseptic",
+    "description": "Bu scripti seviceksin. ",
+    "version": "0.1",
+    "author": "funkydemir66"
+}
+
+ext = Extension(extension_info, sys.argv, silent=True)
+ext.start()
+
+sec_kod = sc = True
+
+def yukle_kod(p):
+    global kod, sec_kod
+
+    if sec_kod:
+        ext.send_to_client('{in:Users}{i:1}{i:6476444}{s:"-Buse."}{s:"Nuvole bianche"}{s:"hr-4117-31-31.hd-627-10.ch-4392-76-1320.lg-710-76.sh-730-76.ha-4354-71-75.ca-4939"}{i:1}{i:3}{i:5}{s:"0.0"}{i:2}{i:1}{s:"f"}{i:64447}{i:3}{s:"um"}{s:""}{i:6269}{b:false}')
+
+
+ext.intercept(Direction.TO_SERVER, yukle_kod, 'GetGuestRoom')
